34e248472aa99d51e488d911a22336f3a98fa08b
Oscar Tu <o.car.w2@gmail.com>
2018-04-13 23:49:59 -0700

    Add debugging/user input options for cities.

--- main.py
+++ main.py
@@ -11,51 +11,68 @@ import ast
 def main():
 	tm = Tour_Manager.Tour_Manager()
 	ga = Genetic_Algorithm.Genetic_Algorithm(tm)
-	list_of_coords = [(60, 200), (180, 200), (80, 180), (140, 180), (20, 160), (100, 160), (200, 160),
-	 					(140, 140), (40, 120), (100, 120), (180, 100), (60, 80), (120, 80), (180, 60),
-	 					(20, 40), (100, 40), (200, 40), (20, 20), (60, 20), (160, 20)]
-	print(list_of_coords)
-	# Create and add our cities
-	city1 = City.City(list_of_coords[0][0], list_of_coords[0][1])
-	tm.add_city(city1)
-	city2 = City.City(list_of_coords[1][0], list_of_coords[1][1])
-	tm.add_city(city2)
-	city3 = City.City(list_of_coords[2][0], list_of_coords[2][1])
-	tm.add_city(city3)
-	city4 = City.City(list_of_coords[3][0], list_of_coords[3][1])
-	tm.add_city(city4)
-	city5 = City.City(list_of_coords[4][0], list_of_coords[4][1])
-	tm.add_city(city5)
-	city6 = City.City(list_of_coords[5][0], list_of_coords[5][1])
-	tm.add_city(city6)
-	city7 = City.City(list_of_coords[6][0], list_of_coords[6][1])
-	tm.add_city(city7)
-	city8 = City.City(list_of_coords[7][0], list_of_coords[7][1])
-	tm.add_city(city8)
-	city9 = City.City(list_of_coords[8][0], list_of_coords[8][1])
-	tm.add_city(city9)
-	city10 = City.City(list_of_coords[9][0], list_of_coords[9][1])
-	tm.add_city(city10)
-	city11 = City.City(list_of_coords[10][0], list_of_coords[10][1])
-	tm.add_city(city11)
-	city12 = City.City(list_of_coords[11][0], list_of_coords[11][1])
-	tm.add_city(city12)
-	city13 = City.City(list_of_coords[12][0], list_of_coords[12][1])
-	tm.add_city(city13)
-	city14 = City.City(list_of_coords[13][0], list_of_coords[13][1])
-	tm.add_city(city14)
-	city15 = City.City(list_of_coords[14][0], list_of_coords[14][1])
-	tm.add_city(city15)
-	city16 = City.City(list_of_coords[15][0], list_of_coords[15][1])
-	tm.add_city(city16)
-	city17 = City.City(list_of_coords[16][0], list_of_coords[16][1])
-	tm.add_city(city17)
-	city18 = City.City(list_of_coords[17][0], list_of_coords[17][1])
-	tm.add_city(city18)
-	city19 = City.City(list_of_coords[18][0], list_of_coords[18][1])
-	tm.add_city(city19)
-	city20 = City.City(list_of_coords[19][0], list_of_coords[19][1])
-	tm.add_city(city20)
+	debug = False
+	if debug:
+		num_cities = int(input("How many cities do you want to add: "))
+		list_of_coords = []
+		for i in range(num_cities):
+			city = input("Enter coordinate x and y separated by a comma: ")
+			while city[0] == "(" or city[-1] == ")":
+				if ',' not in city:
+					city = input("Enter only x and y, and don't forget the comma between the coordinates: ")
+				else:
+					city = input("Try again, just enter x, y (e.g. 10,20; 30, 40; but not (40, 50) ")
+			city = city.split(',')
+			list_of_coords.append((int(city[0]), int(city[1])))
+			city = City.City(int(city[0]), int(city[1]))
+			tm.add_city(city)
+
+	else:
+		list_of_coords = [(60, 200), (180, 200), (80, 180), (140, 180), (20, 160), (100, 160), (200, 160),
+		 					(140, 140), (40, 120), (100, 120), (180, 100), (60, 80), (120, 80), (180, 60),
+		 					(20, 40), (100, 40), (200, 40), (20, 20), (60, 20), (160, 20)]
+		print(list_of_coords)
+		# Create and add our cities
+		city1 = City.City(list_of_coords[0][0], list_of_coords[0][1])
+		tm.add_city(city1)
+		city2 = City.City(list_of_coords[1][0], list_of_coords[1][1])
+		tm.add_city(city2)
+		city3 = City.City(list_of_coords[2][0], list_of_coords[2][1])
+		tm.add_city(city3)
+		city4 = City.City(list_of_coords[3][0], list_of_coords[3][1])
+		tm.add_city(city4)
+		city5 = City.City(list_of_coords[4][0], list_of_coords[4][1])
+		tm.add_city(city5)
+		city6 = City.City(list_of_coords[5][0], list_of_coords[5][1])
+		tm.add_city(city6)
+		city7 = City.City(list_of_coords[6][0], list_of_coords[6][1])
+		tm.add_city(city7)
+		city8 = City.City(list_of_coords[7][0], list_of_coords[7][1])
+		tm.add_city(city8)
+		city9 = City.City(list_of_coords[8][0], list_of_coords[8][1])
+		tm.add_city(city9)
+		city10 = City.City(list_of_coords[9][0], list_of_coords[9][1])
+		tm.add_city(city10)
+		city11 = City.City(list_of_coords[10][0], list_of_coords[10][1])
+		tm.add_city(city11)
+		city12 = City.City(list_of_coords[11][0], list_of_coords[11][1])
+		tm.add_city(city12)
+		city13 = City.City(list_of_coords[12][0], list_of_coords[12][1])
+		tm.add_city(city13)
+		city14 = City.City(list_of_coords[13][0], list_of_coords[13][1])
+		tm.add_city(city14)
+		city15 = City.City(list_of_coords[14][0], list_of_coords[14][1])
+		tm.add_city(city15)
+		city16 = City.City(list_of_coords[15][0], list_of_coords[15][1])
+		tm.add_city(city16)
+		city17 = City.City(list_of_coords[16][0], list_of_coords[16][1])
+		tm.add_city(city17)
+		city18 = City.City(list_of_coords[17][0], list_of_coords[17][1])
+		tm.add_city(city18)
+		city19 = City.City(list_of_coords[18][0], list_of_coords[18][1])
+		tm.add_city(city19)
+		city20 = City.City(list_of_coords[19][0], list_of_coords[19][1])
+		tm.add_city(city20)
 
 
 	# Initialize population
@@ -73,7 +90,7 @@ def main():
 
 	# Evolve population for 100 generations
 	print("Evolving...")
-	for i in range(101):
+	for i in range(251):
 		pop = ga.evolve_population(pop)
 
 	# Print results
